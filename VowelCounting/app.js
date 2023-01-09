36046746699a8c5459d6a530fa565671f96d1670
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-01-09 15:22:55 +0700

    clean code

--- VowelCounting/app.js
+++ VowelCounting/app.js
@@ -2,13 +2,11 @@ const countEl = document.querySelector(".count")
 const input = document.querySelector("input")
 
 input.addEventListener("keyup",()=>{
-    let word = input.value.toLowerCase()
     let vowelCount = 0
-    for(let i = 0;i<word.length;i++){
-        let letter = word[i]
-        if(letter.match(/([a,e,i,o,u])/)){
+    for (let word of input.value){
+        if(word.match(/([aeiou])/i)){
             vowelCount++
         }
     }
-    countEl.innerHTML=`${vowelCount}`
+    countEl.innerText=`${vowelCount}`
 })
