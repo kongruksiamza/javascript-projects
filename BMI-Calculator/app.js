dca83a092df9f7da9078b62b93b3decddfc7d0fe
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-01-16 16:45:06 +0700

    clean code

--- BMI-Calculator/app.js
+++ BMI-Calculator/app.js
@@ -1,33 +1,36 @@
 const btn = document.querySelector(".btn")
 const result = document.querySelector(".result")
+const inputs = document.querySelectorAll('input')
 
-btn.addEventListener("click",(e)=>{
-    e.preventDefault()
-    let weight = document.getElementById("weight").value
-    let height = document.getElementById("height").value
-
-    if(weight === "" || isNaN(weight)){
-        return result.innerHTML = "กรุณาป้อนน้ำหนักของคุณ"
-    }else if(height === "" || isNaN(height)){
-        return result.innerHTML = "กรุณาป้อนส่วนสูงของคุณ"
-    }else{
-        //cm => m
-        height = height/100
-        let bmi = (weight/Math.pow(height,2)).toFixed(2)
-        //บอกเกณฑ์
-        if(bmi<18.5){
-            showResult(bmi,"ผอมเกินไป","blue")
-        }else if(bmi>=18.5 && bmi<=24.9){
-            showResult(bmi,"ปกติ","green")
-        }else if(bmi>=25 && bmi<=29.9){
-            showResult(bmi,"อ้วน","red")
-        }else{
-            showResult(bmi,"อ้วนเกินไป","purple")
-        }
+const calculateBMI = (bmi) => {
+    if (bmi < 18.5) {
+        showResult(bmi, "ผอมเกินไป", "blue")
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        showResult(bmi, "ปกติ", "green")
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        showResult(bmi, "อ้วน", "red")
+    } else {
+        showResult(bmi, "อ้วนเกินไป", "purple")
     }
-})
+}
 
-function showResult(bmi,message,color){
-    result.style.backgroundColor = color
-    return result.innerHTML = `ผลลัพธ์ = ${bmi} (${message})`
+for (let input of inputs) {
+    input.addEventListener(`input`, e => {
+
+        let weight = document.getElementById("weight").value
+        let height = document.getElementById("height").value
+
+        if (weight === "" || isNaN(weight)) {
+            result.innerHTML = "กรุณาป้อนน้ำหนักของคุณ"
+        } else if (height === "" || isNaN(height)) {
+            result.innerHTML = "กรุณาป้อนส่วนสูงของคุณ"
+        } else {
+            calculateBMI((weight / Math.pow(height /= 100, 2)).toFixed(2))
+        }
+    })
+}
+
+function showResult(bmi, msg, color) {
+    result.style.background = color
+    result.innerText = `ผลลัพธ์ = ${bmi} (${msg})`
 }
