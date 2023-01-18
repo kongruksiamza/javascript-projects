5fd49cadf9beac4ef26d79ec5a1b0b7147e1a8b7
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-01-18 16:03:50 +0700

    clean code

--- RandomImage-UnsplashAPI/app.js
+++ RandomImage-UnsplashAPI/app.js
@@ -1,15 +1,12 @@
 const container = document.querySelector(".container")
 const rows = 4
 const cols = 3
+const unsplashRandom = `https://source.unsplash.com/random/`
 
-function randomNumber(){
-    return Math.floor(Math.random()*500)
-}
-for(let i = 0;i<rows*cols;i++){
-    //ภาพ
-    const url = `https://source.unsplash.com/random/${randomNumber()}`
-    //พื้นที่แสดงภาพ
+const randomNumber = () => Math.floor(Math.random() * 500)
+
+for (let i = 0; i < rows * cols; i++) {
     const imageEl = document.createElement("img")
-    imageEl.src=url
+    imageEl.src = `${unsplashRandom}${randomNumber()}`
     container.appendChild(imageEl)
 }
